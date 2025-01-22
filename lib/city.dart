d4d54054ff52ff93ee15ed6325b9e5bc983b2176
Yann <y.baelli05@gmail.com>
2025-01-22 09:24:30 +0100

    Add JSON serialization and Equatable support to City class

--- lib/city.dart
+++ lib/city.dart
@@ -1,13 +1,36 @@
-class City {
+import 'package:equatable/equatable.dart';
+
+class City extends Equatable {
   final String name;
   final int population;
   final Map<String, int> influence;
   final Map<String, int> resources;
 
-  City({
+  const City({
     required this.name,
     required this.population,
     required this.influence,
     required this.resources,
   });
+
+  factory City.fromJson(Map<String, dynamic> json) {
+    return City(
+      name: json['name'],
+      population: json['population'],
+      influence: Map<String, int>.from(json['influence']),
+      resources: Map<String, int>.from(json['resources']),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'population': population,
+      'influence': influence,
+      'resources': resources,
+    };
+  }
+
+  @override
+  List<Object?> get props => [name, population, influence, resources];
 }
